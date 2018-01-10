83310797817f0d557c6a9cecd169316b03824fc4
Ilya Zverev <izverev@logrocon.com>
2018-01-10 15:01:21 +0300

    add gerrit_install

---
+++ gerrit_install.sh
@@ -0,0 +1,16 @@
+﻿sudo apt -y -q update
+sudo apt -y install git
+sudo apt -y install default-jdk
+
+dl=~/Downloads
+if [ ! -d "$dl" ]; then
+   mkdir ~/Downloads
+fi
+cd ~/Downloads
+
+
+wget https://gerrit-releases.storage.googleapis.com/gerrit-2.14.6.war
+
+java -jar gerrit-2.14.6.war init --batch -d ~/gerrit
+
+git config -f ~/gerrit/etc/gerrit.config gerrit.canonicalWebUrl
